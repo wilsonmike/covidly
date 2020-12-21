af3b1aebfce7acadc4fc86d8e9b224c73b6cd870
mike <michaelwilsonsj@hotmail.com>
2020-12-21 17:29:08 -0500

    boilerplate chart display

--- src/api/index.js
+++ src/api/index.js
@@ -10,3 +10,20 @@ export const fetchData = async () => {
     return { confirmed, recovered, deaths, lastUpdate };
   } catch (error) {}
 };
+
+export const fetchDailyData = async () => {
+  try {
+    const { data } = await axios.get(
+      "https://api.covidtracking.com/v1/us/daily.json"
+    );
+
+    return data.map(({ positive, recovered, death, dateChecked: date }) => ({
+      confirmed: positive,
+      recovered,
+      deaths: death,
+      date,
+    }));
+  } catch (error) {
+    return error;
+  }
+};
