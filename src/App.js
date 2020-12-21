e700869d8c1d2c14c6600b117509b4f6db059225
mike <michaelwilsonsj@hotmail.com>
2020-12-20 20:43:43 -0500

    building components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
-import React from 'react';
+import React from "react";
+import { Cards, Chart, CountryPicker } from "./components";
 
 class App extends React.Component {
-    render() {
-        return(
-            <div>
-                <h1>App</h1>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div>
+        <Cards />
+        <Chart />
+        <CountryPicker />
+      </div>
+    );
+  }
 }
 
-export default App; 
+export default App;
