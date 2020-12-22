c4867c2f375b526f4f36b5366b571aea2bc7e9ff
mike <michaelwilsonsj@hotmail.com>
2020-12-22 16:31:12 -0500

    finished bar charts

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import { Cards, Chart, CountryPicker } from "./components";
 import styles from "./App.module.css";
 import { fetchData } from "./api";
 
+import virus from "./images/virus.svg";
+
 class App extends React.Component {
   state = {
     data: {},
@@ -12,13 +14,24 @@ class App extends React.Component {
     const fetchedData = await fetchData();
     this.setState({ data: fetchedData });
   }
+
+  handleCountryChange = async (country) => {
+    const fetchedData = await fetchData(country);
+    this.setState({ data: fetchedData, country: country });
+  };
   render() {
-    const { data } = this.state;
+    const { data, country } = this.state;
     return (
       <div className={styles.container}>
+        <div className={styles.header}>
+        <img className={styles.image} src={virus} alt="Corona Virus" />
+        <h1>Covidly</h1>
+        <h2 className={styles.smalltxt}>Covid-19 Tracker</h2>
+        </div>
+        
         <Cards data={data} />
-        <CountryPicker />
-        <Chart />
+        <CountryPicker handleCountryChange={this.handleCountryChange} />
+        <Chart data={data} country={country} />
       </div>
     );
   }
